dc2573fea17e7f96d110613c149fd110f6a4a4c8
Sourav Chhimpa <souravchhimpa1@gmail.com>
2024-09-09 23:43:37 +0530

    navbar created

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased px-3 dark`}
       >
+        <div className="rleative w-full flex items-center justify-center">
         <Navbar/>
+        </div>
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
+    <>
     <div className="w-full h-screen flex items-center justify-center">
     <h1 className="font-semibold text-center text-gray-600 text-2xl">Say Hi to <span className="block uppercase text-blue-400">sourav</span></h1>
     </div>
+    <div className="w-full h-screen"></div>
+    </>
   );
 }
