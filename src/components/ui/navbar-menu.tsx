7e7b4baf953312243ac4c92d079b6b46a6b483b6
Sourav Chhimpa <souravchhimpa1@gmail.com>
2024-09-09 23:53:31 +0530

    Few bugs are fixed for the navbar-menu.tsx to fixed the bug from the vercel production

--- src/components/ui/navbar-menu.tsx
+++ src/components/ui/navbar-menu.tsx
@@ -1,9 +1,13 @@
 "use client";
 import React from "react";
 import { motion } from "framer-motion";
-import Link from "next/link";
+import Link, {LinkProps} from "next/link";
 import Image from "next/image";
 
+interface HoveredLinkProps extends LinkProps {
+    children: React.ReactNode;
+  }
+  
 const transition = {
   type: "spring",
   mass: 0.5,
@@ -109,13 +113,13 @@ export const ProductItem = ({
   );
 };
 
-export const HoveredLink = ({ children, ...rest }: any) => {
-  return (
-    <Link
-      {...rest}
-      className="text-neutral-700 dark:text-neutral-200 hover:text-black "
-    >
-      {children}
-    </Link>
-  );
-};
+export const HoveredLink = ({ children, ...rest }: HoveredLinkProps) => {
+    return (
+      <Link
+        {...rest}
+        className="text-neutral-700 dark:text-neutral-200 hover:text-black"
+      >
+        {children}
+      </Link>
+    );
+  };
