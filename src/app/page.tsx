5666dfdd1bb43488cd86bed345c924c430ddf64c
Sourav Chhimpa <souravchhimpa1@gmail.com>
2024-09-09 22:21:10 +0530

    few files are created for th e future task

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="w-full h-screen flex items-center justify-center">
-    <h1 className="font-semibold text-center text-gray-600 text-2xl">Hello Say Hi to <span className="block uppercase text-blue-400">sourav</span></h1>
+    <h1 className="font-semibold text-center text-gray-600 text-2xl">Say Hi to <span className="block uppercase text-blue-400">sourav</span></h1>
     </div>
   );
 }
