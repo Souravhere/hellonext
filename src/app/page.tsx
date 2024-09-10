333b3cdb36606e1644003f0c41bfd8f61a1cab4b
Sourav Chhimpa <souravchhimpa1@gmail.com>
2024-09-10 18:42:59 +0530

    Hero section will created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
+import { TypewriterEffectSmoothDemo } from "@/components/Herosection";
+
 export default function Home() {
   return (
     <>
-    <div className="w-full h-screen flex items-center justify-center">
-    <h1 className="font-semibold text-center text-gray-600 text-2xl">Say Hi to <span className="block uppercase text-blue-400">sourav</span></h1>
-    </div>
+    <TypewriterEffectSmoothDemo/>
     <div className="w-full h-screen"></div>
     </>
   );
